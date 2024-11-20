e45c0f1c1e4a307d4d5d793191f9c592d61cef19
DiskKun <luca5h1905@gmail.com>
2024-11-19 20:19:18 -0500

    week 10 tasks

--- Hourahine_Platformer/Assets/Scripts/PlayerController.cs
+++ Hourahine_Platformer/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    float xVelocity = 0;
+
+    Rigidbody2D rb;
+    public float horizontalMaxVelocity = 10;
+    public float horizontalAcceleration = 1;
+    public float horizontalDeceleration = 1;
+
+    public float groundedRaycastLength;
+
     public enum FacingDirection
     {
         left, right
@@ -12,34 +22,75 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        xVelocity += horizontalAcceleration * playerInput.x;
+        if (playerInput.x == 0 && xVelocity != 0)
+        {
+            xVelocity += -(Mathf.Abs(xVelocity) / xVelocity) * horizontalDeceleration;
+            if (xVelocity > -1f && xVelocity < 1f)
+            {
+                xVelocity = 0;
+            }
+        }
 
+        xVelocity = Mathf.Clamp(xVelocity, -horizontalMaxVelocity, horizontalMaxVelocity);
+
+        rb.MovePosition(rb.position + new Vector2(xVelocity, 0) * Time.deltaTime);
     }
 
     public bool IsWalking()
     {
-        return false;
+        if (xVelocity != 0)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
     }
     public bool IsGrounded()
     {
-        return false;
+        gameObject.layer = 2;
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * groundedRaycastLength);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundedRaycastLength);
+        gameObject.layer = 7;
+        if (hit.collider != null)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        if (xVelocity > 0)
+        {
+            return FacingDirection.right;
+
+        }
+        else if (xVelocity < 0)
+        {
+            return FacingDirection.left;
+
+        } else
+        {
+            return FacingDirection.left;
+        }
+      
     }
 }
