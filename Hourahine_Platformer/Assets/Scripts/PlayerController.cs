568eef6f5fd0e7906c8af1f4f3a67698e4dcacaa
DiskKun <luca5h1905@gmail.com>
2024-11-23 23:24:56 -0500

    Week 11 Commit

--- Hourahine_Platformer/Assets/Scripts/PlayerController.cs
+++ Hourahine_Platformer/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,16 @@ public class PlayerController : MonoBehaviour
 {
 
     float xVelocity = 0;
+    float yVelocity = 0;
+    float jumpVelocity;
+    float gravity;
 
+
+    public float apexTime;
+    public float apexHeight;
+    public float terminalVelocity;
+    public float coyoteTime;
+    float coyoteTimer;
     Rigidbody2D rb;
     public float horizontalMaxVelocity = 10;
     public float horizontalAcceleration = 1;
@@ -22,12 +31,15 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        gravity = -2 * apexHeight / Mathf.Pow(apexTime, 2);
+        jumpVelocity = 2 * apexHeight / apexTime;
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
@@ -36,10 +48,10 @@ public class PlayerController : MonoBehaviour
 
     private void MovementUpdate(Vector2 playerInput)
     {
-        xVelocity += horizontalAcceleration * playerInput.x;
+        xVelocity += horizontalAcceleration * playerInput.x * Time.deltaTime;
         if (playerInput.x == 0 && xVelocity != 0)
         {
-            xVelocity += -(Mathf.Abs(xVelocity) / xVelocity) * horizontalDeceleration;
+            xVelocity += -(Mathf.Abs(xVelocity) / xVelocity) * horizontalDeceleration * Time.deltaTime;
             if (xVelocity > -1f && xVelocity < 1f)
             {
                 xVelocity = 0;
@@ -48,7 +60,35 @@ public class PlayerController : MonoBehaviour
 
         xVelocity = Mathf.Clamp(xVelocity, -horizontalMaxVelocity, horizontalMaxVelocity);
 
-        rb.MovePosition(rb.position + new Vector2(xVelocity, 0) * Time.deltaTime);
+        if (IsGrounded())
+        {
+            yVelocity = 0;
+            coyoteTimer = coyoteTime;
+        } else
+        {
+            coyoteTimer -= Time.deltaTime;
+        }
+
+        if (yVelocity < terminalVelocity)
+        {
+            yVelocity = terminalVelocity;
+        }
+
+
+
+        if (playerInput.y == 1 && (IsGrounded() || coyoteTimer > 0))
+        {
+            yVelocity = jumpVelocity;
+            coyoteTimer = 0;
+        }
+
+
+
+        rb.MovePosition(rb.position + new Vector2(xVelocity, yVelocity) * Time.deltaTime);
+
+        yVelocity += gravity * Time.deltaTime;
+
+
     }
 
     public bool IsWalking()
