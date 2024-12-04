1a6bfb2a75656e683ab7244ce4fbbd10542c7daa
DiskKun <luca5h1905@gmail.com>
2024-12-03 22:09:37 -0500

    Update PlayerController.cs
    add comments to code.

--- Hourahine_Platformer/Assets/Scripts/PlayerController.cs
+++ Hourahine_Platformer/Assets/Scripts/PlayerController.cs
@@ -143,24 +143,30 @@ public class PlayerController : MonoBehaviour
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        // handle horizontal acceleration
         xVelocity += horizontalAcceleration * playerInput.x * Time.deltaTime;
 
+
+        // apply friction
         if (playerInput.x == 0 && xVelocity != 0 && IsGrounded())
         {
             xVelocity += -(Mathf.Abs(xVelocity) / xVelocity) * horizontalDeceleration * Time.deltaTime;
+            // stop player
             if (xVelocity > -1f && xVelocity < 1f)
             {
                 xVelocity = 0;
             }
         }
 
+
+        // when not dashing, and while in air, apply the max horizontal speed
         if (dashTimer < 0 || IsGrounded())
         {
             xVelocity = Mathf.Clamp(xVelocity, -horizontalMaxVelocity, horizontalMaxVelocity);
 
         }
 
-
+        // if not on the ground, decrease the coyoteTimer. otherwise, set it to the starting timer value
         if (IsGrounded())
         {
             yVelocity = 0;
@@ -170,6 +176,7 @@ public class PlayerController : MonoBehaviour
             coyoteTimer -= Time.deltaTime;
         }
 
+        // terminal velocity changes depending on whether or not you are sliding against a wall
         float newTerminalVelocity;
 
         if (IsHittingWall())
@@ -179,33 +186,37 @@ public class PlayerController : MonoBehaviour
         {
             newTerminalVelocity = terminalVelocity;
         }
-
+        
+        // apply terminal velocity
         if (yVelocity < newTerminalVelocity)
         {
             yVelocity = newTerminalVelocity;
         }
 
 
-
+        // check to see if you can jump, and apply jumpvelocity if true
+        // checks to see if up is being pressed, and the player is grounded OR if they coyotetimer hasn't ran out, OR if you are hitting a wall and sliding down it.
         if (playerInput.y == 1 && (IsGrounded() || coyoteTimer > 0 || (IsHittingWall() && yVelocity < 0)))
         {
             yVelocity = jumpVelocity;
             coyoteTimer = 0;
         }
 
+        // decrease the dashtimer
         dashTimer -= Time.deltaTime;
-        Debug.Log(dashTimer);
 
+        // apply the dash boost when you press the dash button
         if (dashButton && dashTimer <= 0)
         {
-            Debug.Log("dash!");
             dashTimer = dashTime;
             yVelocity = playerInput.y * dashStrength;
             xVelocity = xVelocity * dashStrength;
         }
 
+        //move the player
         rb.MovePosition(rb.position + new Vector2(xVelocity, yVelocity) * Time.deltaTime);
 
+        // apply gravity
         yVelocity += gravity * Time.deltaTime;
 
 
@@ -227,7 +238,8 @@ public class PlayerController : MonoBehaviour
         }
     }
     public bool IsGrounded()
-    {        
+    {
+        // perform a raycast to see if the player is touching the ground.
         Debug.DrawLine(transform.position, transform.position + Vector3.down * groundedRaycastLength);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundedRaycastLength, groundedMask);
         if (hit.collider != null)
@@ -241,6 +253,7 @@ public class PlayerController : MonoBehaviour
 
     public bool IsHittingWall()
     {
+        //perform two raycasts (one left, one right) to see if the player is hitting a wall horizontally
         Debug.DrawLine(transform.position, transform.position + Vector3.right * groundedRaycastLength);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, groundedRaycastLength, groundedMask);
         RaycastHit2D hit2 = Physics2D.Raycast(transform.position, Vector2.left, groundedRaycastLength, groundedMask);
